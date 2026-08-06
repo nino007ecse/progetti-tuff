fd7ef3fc44e3f4647b1f3b308abc01fbfa876131
ninja <296844581+nino007ecse@users.noreply.github.com>
2026-08-06 11:11:03 +0200

    Add eta.c

---
+++ ninja/c/eta.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int età;
+
+    printf("Inserisci la tua età: ");
+    scanf("%d", &età);
+
+    if (età < 0) {
+        printf("L'età inserita non è valida\n");
+    }
+
+    else if (età >= 18) {
+        printf("Lo studente è maggiorenne\n");
+    }
+    else {
+        printf("Lo studente è minorenne\n");
+    }
+
+    return 0;
+}
